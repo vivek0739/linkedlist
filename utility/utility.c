68c55dc1540695ac2570f356e85e864c360163ae
vivek0739 <vivek0739@gmail.com>
2014-08-15 18:41:06 +0530

    first commit

--- utility/utility.c
+++ utility/utility.c
@@ -6,6 +6,38 @@ struct node
 	struct node *next;
 };
 /********************************utility function********************************************************************************/
+void print_middle(struct node *head)
+{
+	struct node *fast_ptr=head;
+	struct node *second =head;
+	if(head!=NULL)
+	{
+		while(fast_ptr!=NULL && fast_ptr->next!=NULL)
+		{
+			fast_ptr=fast_ptr->next->next;
+			second=second->next;
+			
+		}
+		printf("middle=%d\n",second->data);
+	}
+}
+
+
+int nth_node(struct node *head,int n)
+{
+	int count=0;
+		while(head!=NULL)
+		{
+			count++;
+			if(count == n)
+				return head->data;
+			
+		  head=head->next;	
+		}
+		
+			
+}
+
 void del_ll(struct node **head,int key)
 {
 	struct node *ptr=*head,*prev;;
@@ -20,17 +52,20 @@ void del_ll(struct node **head,int key)
 		free(ptr);
 		return;
 	}
-	while(ptr->next != NULL && ptr->next->data != key)
+	while(ptr != NULL && ptr->data != key)
+	{
+		prev=ptr;
 		ptr=ptr->next;
-	if(ptr->next == NULL)
+	}
+	if(ptr == NULL)
 	{
 		printf("item not found\n");
 		return;
 	}
 	else
 	{
-		ptr->next=ptr->next->next;
-		free(ptr->next);
+		prev->next=ptr->next;
+		free(ptr);
 	}
 
 }
@@ -66,6 +101,7 @@ void insert_end(struct node **head,int data)
 	
 	newnode=(struct node *)malloc(sizeof(struct node));
 	newnode->data=data;
+	newnode->next=NULL;
 	if(ptr == NULL)
 	{
 		newnode->next=(*head);
@@ -99,9 +135,12 @@ int main()
 	push_ll(&head,2);
 	push_ll(&head,6);
 	print_ll(head);
+	
 	insert_after(&(head->next),9);
 	print_ll(head);
-	del_ll(&head,5);
+	print_middle(head);
+	printf("%d \n",nth_node(head,3));
+	del_ll(&head,9);
 	print_ll(head);
 	insert_end(&head,7);
 	print_ll(head);

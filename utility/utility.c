1ff8119f7beacaa1ad69d7a8d09c314bd602bf29
vivek0739 <vivek0739@gmail.com>
2014-08-17 18:08:36 +0530

    first commit

--- utility/utility.c
+++ utility/utility.c
@@ -9,10 +9,275 @@ struct node
 };
 bool iscompare(struct node*,struct node*);
 void reverse_ll(struct node**);
+struct node* merge(struct node* , struct node* );
+void frontbacksplit(struct node*,struct node**,struct node**);
+
 /********************************testing*****************************************************************************************/
+//quick sort
+struct node *gettail(struct node *head)
+{
+	if(head != NULL )
+	{
+		while (head -> next != NULL )
+		{	
+			head = head -> next;
+		}
+	}
+	return head;
+}
+struct node *partition(struct node *head,struct node *end, struct node **newhead ,struct node **newend)
+{
+	
 
+	struct node *pivot = end;
+	struct node *prev = NULL;
+	struct node *cur = head , *tail = pivot;
+	while( cur != pivot)
+	{
+		if(cur -> data < pivot -> data)
+		{
+			if((*newhead) == NULL)
+			(*newhead) = cur;
+			prev = cur;
+			cur = cur -> next;
+		}
+		else
+		{
+			if(prev)
+				prev -> next = cur -> next;
+			struct node *tmp = cur -> next;
+			cur -> next = NULL;
+			tail -> next = cur;
+			tail = cur;
+			cur = tmp;
+		}
+	}
+	if((*newhead) == NULL)
+		(*newhead) = pivot;
+	(*newend) = tail;
+	return pivot;
 
+}
+struct node *quicksortrecur(struct node *head ,struct node *end)
+{
+	if(head == NULL || head == end )
+	return head;
+	struct node *newhead = NULL , *newend = NULL;
+	struct node *pivot = partition(head , end ,&newhead , &newend);
+	if(newhead != pivot)
+		{
+			struct node *tmp = newhead;
+			while(tmp -> next != pivot)
+				tmp =tmp -> next;
+			tmp -> next = NULL;
+			newhead = quicksortrecur(newhead , tmp);
+			tmp = gettail(newhead);
+			tmp -> next = pivot;
+		}
+	pivot ->next = quicksortrecur(pivot -> next, newend);
+	return newhead;
+}
+void quicksort(struct node **headref )
+{
+	(*headref) = quicksortrecur(*headref , gettail(*headref));
+	return;
+}
 /********************************utility function********************************************************************************/
+void swap(int *a,int *b)
+{
+	int temp;
+	temp=*a;
+	*a=*b;
+	*b=temp;
+
+}
+
+void swap_ll(struct node *head)
+{
+	while(head != NULL && head->next != NULL)
+	{
+		swap(&head->data,&head->next->data);
+		head=head->next->next;
+	}
+}
+
+
+void del_dup(struct node *head)
+{
+	struct node *next=NULL;
+	if(head == NULL)
+		return;
+	while( head -> next != NULL)
+	{
+		if(head->data == head->next->data)
+		{
+			next=head->next->next;
+			free(head->next);
+			head->next=next;		
+		}
+		else
+			head=head->next;
+	}
+}
+
+//print reverse
+void print_rev(struct node *head)
+{
+	if(head == NULL)
+		return;
+	print_rev(head->next);
+	printf("%d ",head->data);
+}
+
+//intersecting  point two ll
+int _get_intersecting(int ,struct node*,struct node*);
+int getcount(struct node *head)
+{
+	int cont = 0;
+	while(head!=NULL)
+	{
+		cont++;
+		head=head->next;
+	}
+return cont;
+}
+int get_intersecting(struct node *head1,struct node *head2)
+{
+	int c1=getcount(head1);
+	int c2=getcount(head2);
+	int d;
+	
+	if(c1>c2)
+	{
+		d=c1-c2;
+		 return _get_intersecting(d,head1,head2);
+	}
+	else
+	{
+		d=c2-c1;
+		 return _get_intersecting(d,head2,head1);
+		
+	}
+
+}
+int _get_intersecting(int d,struct node *head1,struct node *head2)
+{
+	int i;
+	for(i=0;i<d;i++)
+	{
+		if(head1 == NULL)
+			return -1;
+		head1=head1->next;
+	}
+//printf("%d %d",head1->data,head2->data);
+	while(head1!= NULL && head2 != NULL)
+	{
+		if(head1== head2)
+		return head1->data;
+		head1=head1->next;
+		head2=head2->next;
+	}
+return -1;
+}
+
+void insert_sort(struct node **head,int data)
+{
+	struct node *newnode=(struct node*)malloc(sizeof(struct node));
+	newnode->data = data;
+	newnode->next= NULL;
+	if(*head == NULL)
+	{
+		*head=newnode;
+		return;
+
+	}
+	else if((*head)->data>=data)
+	{
+		newnode->next=(*head);
+		(*head)=newnode;
+	}
+	else
+	{
+		struct node *ref=*head;
+		while(ref ->next !=NULL && ref->next->data < data )
+		{
+			ref=ref->next;
+
+		}
+		newnode->next=ref->next;
+		ref->next=newnode;
+	}
+
+}
+
+
+void merge_sort(struct node **headref)
+{
+	struct node *head=*headref;
+	struct node *a= NULL;
+	struct node *b = NULL;
+	
+	if(head == NULL || head -> next == NULL)
+	{
+		return;
+	}
+	frontbacksplit(head,&a,&b);
+	merge_sort(&a);
+	merge_sort(&b);
+	*headref = merge(a , b);
+
+	
+	
+}
+void frontbacksplit(struct node *head,struct node **a,struct node **b)
+{
+	if( head == NULL || head -> next == NULL)
+	{
+		*a = head;
+		*b = NULL;
+
+	}
+	else
+	{
+		struct node *fast;
+		struct node *slow;
+		slow = head;
+    		fast = head->next;
+		while(fast != NULL)
+		{
+			fast = fast -> next;
+			if(fast != NULL)
+			{
+				slow = slow -> next;
+				fast = fast -> next;
+
+
+			}
+		}
+		*a = head;
+		*b = slow -> next;
+		slow -> next = NULL;
+	}
+}
+
+struct node* merge(struct node *a,struct node *b)
+{
+	struct node *result = NULL;
+	if(a==NULL) return b;
+	if(b==NULL) return a;
+	if(a->data <= b->data)
+	{
+		result = a;
+		result->next = merge(a->next , b);
+	}
+	else
+	{
+		result = b;
+		result -> next = merge(a, b->next);
+	}
+	return result;
+}
+
 
 bool ispallindrom(struct node **head)
 {
@@ -268,8 +533,25 @@ int main()
 	print_ll(head);
 	insert_end(&head,7);
 	print_ll(head);
-	printf("%d l\n",detect_loop(head));*/
+	printf("%d l\n",detect_loop(head));
 		printf("%d l\n",ispallindrom(&head));
+	struct node *head2=NULL;
+	push_ll(&head2,5);
+	push_ll(&head2,3);
+	print_ll(head2);
+	print_rev(head);
+	head->next->next=head2;
+	printf("%d l\n",get_intersecting(head,head2));
+	merge_sort(&head);
+	print_ll(head);
+	insert_sort(&head,4);
+	insert_sort(&head,4);
+	insert_sort(&head,4);
+	print_ll(head);
+	del_dup(head);
+	swap_ll(head);*/
+	quicksort(&head);
+		print_ll(head);
 
 return 0;
 }
